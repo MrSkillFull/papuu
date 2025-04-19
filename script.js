aad18c6977a47c5e43368466a554bf3c985b31cd
Fernando Lima <fernandoassisoliveiralima@gmail.com>
2025-04-18 21:11:04 -0300

    Footer consertado e adicionada tela de registro de novos usuários.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
+//animação da tela de login
 spanPapuu = document.querySelector('.greetings span');
-
-
-
 setInterval(() => {
     if(spanPapuu.style.visibility == 'hidden'){
         spanPapuu.style.visibility = 'visible';
@@ -9,3 +7,16 @@ setInterval(() => {
         spanPapuu.style.visibility = 'hidden'
     }
 }, 750);
+
+//Mostrar tela de criação de conta ao clicar em registrar
+registBtn = document.querySelector('.regist');
+registBtn.addEventListener('click', ()=>{
+    let createAccountSection = document.querySelector('section.create-account');
+    createAccountSection.style.display = 'flex';
+})
+//Fechar tela de criação de conta ao clicar em fechar
+var close = document.querySelector('.close');
+close.addEventListener('click', ()=>{
+    let createAccountSection = document.querySelector('section.create-account');
+    createAccountSection.style.display = 'none';
+})
